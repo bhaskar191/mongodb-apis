0f85d72a478ee565296315e16364bfd20978000f
bhaskar191 <bhaskar.kumbar@wielandsolutions.com>
2018-11-02 11:35:54 +0530

    added mongodb deletion method

---
+++ mongodb/mongodb-delete.js
@@ -0,0 +1,26 @@
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:23516/bhaskar',{ useNewUrlParser: true },(err,client) =>{
+    if(err){
+        console.log('Unable to Connect the Mongodb Server',err);
+    }
+    console.log('Connected to Mongodb Server');
+
+    const db = client.db('bhaskar');
+    //Delete Many                                                                                               
+    // db.collection('bhaskar').deleteMany({company:'microsoft'}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    //Delete One
+    // db.collection('bhaskar').deleteOne({company: '123'}).then((result) =>{
+    //     console.log(JSON.stringify(result,undefined,2));
+    //     });
+    
+    // //findOneAndDelete
+    db.collection('bhaskar').findOneAndDelete({company:'123'}).then((result) =>{
+        console.log(result);
+    }); 
+    //client.close
+});
+
