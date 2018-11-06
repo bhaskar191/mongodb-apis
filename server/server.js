f54d4ee5642e6597044ee14f9383798a78ea8afa
bhaskar191 <bhaskar.kumbar@wielandsolutions.com>
2018-11-06 12:10:32 +0530

    Get mehod Added

--- server/server.js
+++ server/server.js
@@ -23,9 +23,18 @@ app.post('/todos', (req , res ) =>{
     });
 });
 
+app.get('/todos',(req , res) =>{
+    Todo.find().then((todos) =>{
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(5000, () =>{
     console.log('Started Server at:5000')
 });
 
 
 
+module.exports = {app};
