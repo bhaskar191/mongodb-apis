b9cc6ed99e02628df11c8fbe2cbb59e15a7ce1e0
bhaskar191 <bhaskar.kumbar@wielandsolutions.com>
2018-11-06 15:32:44 +0530

    enhanced Todo's with GET request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -30,6 +31,24 @@ app.get('/todos',(req , res) =>{
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id',(req,res) =>{
+var id  = req.params.id;
+
+if(!ObjectId.isValid(id)){
+    res.status(400).send();
+}
+
+Todo.findById(id).then((todo) =>{
+    if(!Todo){
+        res.status(404).send();
+    }
+    res.send({todo});
+}).catch((e) => {
+    res.status(400).send();
+});
+});
+
 
 app.listen(5000, () =>{
     console.log('Started Server at:5000')
